4a6e24ef0374eb0fdb4b8bface55e8c2cb9c95bb
AadityaSurya <aaditya.surya0@gmail.com>
2024-08-26 04:03:08 -0700

    Enabled vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
+  <Analytics />
 }
