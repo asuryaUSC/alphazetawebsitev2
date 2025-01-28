1b51b2510489bc02086173723b6573ccb7029f15
Aaditya Surya <asurya@usc.edu>
2025-01-27 22:28:26 -0800

    speed insights

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@ import "@/styles/globals.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
   <Analytics />
+
 }
