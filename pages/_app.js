dcef4e2b7083da617fb7851a0189c07d443c2781
Aaditya Surya <asurya@usc.edu>
2025-01-27 22:31:32 -0800

    speed insight bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-  <Analytics />
-
+  return (
+    <>
+      <SpeedInsights />
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
